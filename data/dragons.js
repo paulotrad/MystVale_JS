28a6a9857fc783649034440eb69a85b56edabf2a
Paul Otradovec <you@example.com>
2025-06-07 23:56:36 -0400

    added brunstide model basic and added the front card

--- data/dragons.js
+++ data/dragons.js
@@ -16,7 +16,7 @@ const dragons = [
     name: "Bruntide",
     class: "Brave",
     description:
-      "Bruntide wasn’t born with flame in his lungs or glory in his name...",
+      "Bruntide wasn’t born with fire in his chest or fame in his name. Raised beneath the Scorchspire by a family of flour-dusted bakers, he surprised everyone when he entered the Ember Trial — a perilous rite of passage where young dragons face the Five Fires of Courage. He braved the whispering Flames of Fear, crossed the vanishing Bridge of Smoke, and faced himself in the Ash Mirror — a battle not of muscle, but of will. His snout bears a scar from that fight, a mark not of failure, but of quiet strength. Now Bruntide travels Mystvale with a flame-marked shield and a heart forged in trials. He protects the small and the scared — not with roars, but with steady courage. When danger rises, children whisper his name and search the skies for the shadow of a dragon who walks into fear, not away from it. Because Bruntide knows: true bravery isn’t loud. It just shows up.",
     image: "/dragons/bruntide.png",
      card: [ '/cards/bruntide_front.png','/cards/bruntide_back.png'],
     _3dModels: ['/models/brunswick.glb']
