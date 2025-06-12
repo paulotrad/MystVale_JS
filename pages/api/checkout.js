e22519415e6abff9d8aac14bce3bc016a29d9c10
Paul Otradovec <you@example.com>
2025-06-12 00:34:15 -0400

    added counter and tracker

--- pages/api/checkout.js
+++ pages/api/checkout.js
@@ -17,8 +17,8 @@ export default async function handler(req, res) {
         },
       ],
       mode: 'payment',
-      success_url: `${req.headers.origin}`,
-      cancel_url: `${req.headers.origin}/cancel`,
+      success_url: `https://myst-vale-js.vercel.app/`,
+      cancel_url: `https://myst-vale-js.vercel.app/cancel`,
        shipping_address_collection: {
     allowed_countries: ['US', 'CA'], // You can list all allowed countries
   },
